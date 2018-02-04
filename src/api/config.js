8584b5a7cb611957d9d49724b8cdff2df4440e1a
osmandi <osmandi.github@gmail.com>
2018-02-04 08:28:36 -0400

    First commit

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {    
+    appName:'platzimusic',
+    apiKey: 'd04360770a05685284594a2995aed9c1',
+    secret: '8a8f5bc06de45a7cdb95801232af042b',
+    registredTo: 'osmandi1'
+}
---
+++ src/components/Artist.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+    li.artist
+        h2: a(:href="artist.url" target="_blank") {{ artist.name  }}
+        img(:src="artist.image[2]['#text']")
+</template>
+
+<script>
+export default {
+    name: 'artist',
+    props: [ 'artist' ]
+}
+// Con scope es para poner id a los elementos en CSS
+</script>
+
+<style lang="stylus" scoped>
+li.artist
+    display block
+    margin 10px 0
+</style>
